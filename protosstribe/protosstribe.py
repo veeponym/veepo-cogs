5456505840df9afa7800bee419ee98b7f70cafe0
Dillon Smith <dillon.th.smith@gmail.com>
2020-10-13 18:07:29 -0400

    testing member roles

--- protosstribe/protosstribe.py
+++ protosstribe/protosstribe.py
@@ -104,6 +104,7 @@ class ProtossTribe(commands.Cog):
         await member_config.tribe.set(tribe)
         tribes = ["Khalai","Nerazim","Tal'darim","Purifiers"]
         print([i for i in tribes if i in member.roles])
+        print(member.roles)
         await ctx.send(embed=embed)
 
     def cog_unload(self):
